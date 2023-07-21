b69c27a359f895db1d4041ab477717cf6d7f35a4
Mian Usama Ijaz <106165960+MianUsamaIjaz@users.noreply.github.com>
2023-07-21 10:59:27 -0230

    Update app.js to host it with Render

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://secrets-lcyt.onrender.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
